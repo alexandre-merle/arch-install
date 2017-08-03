36cf617788cad0775c9c870d201d4d8b7d2a5e70
mvaude <maxime.vaude@scality.com>
2017-08-03 15:53:06 +0200

    improve chroot_install script with defensive programming notions

--- chroot-install.sh
+++ chroot-install.sh
@@ -2,55 +2,75 @@
 
 set -ex
 
-device=$1
-group=$2
+local DEVICE=$1
+local GROUP=$2
 
-mkdir /run/lvm
-mount --bind /hostrun/lvm /run/lvm
+lvm_mount() {
+  mkdir /run/lvm
+  mount --bind /hostrun/lvm /run/lvm
+}
 
-# setting timezone
-ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
-hwclock --systohc
+set_locales() {
+  # setting timezone
+  ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
+  hwclock --systohc
 
-# setting en_US locale
-sed -i s/#en_US.UTF-8/en_US.UTF-8/g /etc/locale.gen
-# generating locales
-locale-gen
+  # setting en_US locale
+  sed -i "s/#en_US.UTF-8/en_US.UTF-8/g" /etc/locale.gen
+  # generating locales
+  locale-gen
 
-# setting hostname
-echo "${group}-computer" > /etc/hostname
+  # setting hostname
+  echo "${GROUP}-computer" > /etc/hostname
 
-# adding hostname to hosts
-cat >> /etc/hosts <<EOF
-127.0.1.1	${group}-computer.localdomain ${group}-computer
-EOF
+  # adding hostname to hosts
+  cat >> /etc/hosts << EOF
+  127.0.1.1	${GROUP}-computer.localdomain ${GROUP}-computer
+  EOF
+}
 
-# setting root password
-passwd
+root_install() {
+  # setting root password
+  passwd
 
-pacman -Sy --noconfirm grub efibootmgr
+  pacman -Sy --noconfirm grub efibootmgr
+}
 
-# configuration of grub for encryption on top of lvm / volume
-echo "GRUB_CMDLINE_LINUX=\"cryptdevice=${device}3:base\"" >> /etc/default/grub
-echo "GRUB_ENABLE_CRYPTODISK=y" >> /etc/default/grub
+boot_install() {
+  # configuration of grub for encryption on top of lvm / volume
+  echo "GRUB_CMDLINE_LINUX=\"cryptDEVICE=${DEVICE}3:base\"" >> /etc/default/grub
+  echo "GRUB_ENABLE_CRYPTODISK=y" >> /etc/default/grub
 
-# add hooks to mkinitcpio
-echo "HOOKS=\"base udev autodetect keyboard keymap modconf block encrypt lvm2 filesystems fsck\"" >> /etc/mkinitcpio.conf
+  # add hooks to mkinitcpio
+  echo "HOOKS=\"base udev autodetect keyboard keymap modconf block encrypt lvm2 filesystems fsck\"" >> /etc/mkinitcpio.conf
 
-mkinitcpio -p linux
+  mkinitcpio -p linux
 
-grub-mkconfig -o /boot/grub/grub.cfg
-mkdir -p /boot/efi/EFI
-grub-install --recheck --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=arch_grub
+  grub-mkconfig -o /boot/grub/grub.cfg
+  mkdir -p /boot/efi/EFI
+  grub-install --recheck --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=arch_grub
+}
 
-umount /run/lvm
+finish() {
+  umount /run/lvm
 
-iethernet=`ip addr | grep '[0-9]: en' | cut -d ':' -f 2 | tr -d ' '`
+  iethernet=`ip addr | grep '[0-9]: en' | cut -d ':' -f 2 | tr -d ' '`
 
-for interface in $iethernet; do
-systemctl enable dhcpcd@$interface
-systemctl start dhcpcd@$interface
-done
+  for interface in $iethernet; do
+  systemctl enable dhcpcd@$interface
+  systemctl start dhcpcd@$interface
+  done
 
-# leaving chroot env
-exit
+  # leaving chroot env
+  exit
+}
+
+main() {
+  lvm_mount
+  set_locales
+  root_install
+  boot_install
+  finish
+}
+
+main
