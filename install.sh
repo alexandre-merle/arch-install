416faa5eb43c36e371c635e48cced31add8b784a
mvaude <maxime.vaude@scality.com>
2017-08-03 15:44:56 +0200

    improve install script with defensive programming notions

--- install.sh
+++ install.sh
@@ -2,93 +2,121 @@
 
 set -ex
 
-ping -c 1 www.google.com
-dest='/mnt'
-device=$1
-group=$2
-reset=false
-if [ ! -z "$3" ]; then
-	reset=true
-fi
+readonly PROGNAME=$(basename $0)
+readonly DEST="/mnt"
+readonly DEVICE=$1
+readonly GROUP=$2
 
-if [ -z "$1" ] || [ -z "$2" ]; then
-	echo "usage: ${0} <device> <group>"
-	exit 1
-fi
+test_connection() {
+  ping -c 1 www.google.com
+}
 
-if [ reset ]; then
-	curl -O https://raw.githubusercontent.com/alexandre-merle/arch-install/master/install_reset.sh && bash install_reset.sh $1 $2
-fi
+check_args() {
+  if [ -z $DEVICE ] || [ -z $GROUP ]; then
+	  echo "usage: ./$PROGNAME <DEVICE> <GROUP>"
+	  exit 1
+  fi
 
-# destroying mbr or gpt
-sgdisk -Z $device
-# creating new gpt table
-sgdisk -og $device
-# refresh os table partition information
-partprobe $device
-# setup partitions
-sgdisk -n 1:2048:1026047 -c 1:"EFI System Partition" -t 1:ef00 $device
-sgdisk -n 2:1026048:3074047 -c 2:"Linux /boot" -t 2:8300 $device
-# get end of disk
-end_sector=`sgdisk -E ${device}`
-sgdisk -n 3:3074048:${end_sector} -c 3:"Linux LVM" -t 3:8e00 $device
-# print partition table
-sgdisk -p ${device}
+  if [ -z "$3" ]; then
+	  curl -O https://raw.githubusercontent.com/alexandre-merle/arch-install/master/install_reset.sh && bash install_reset.sh $DEVICE $GROUP
+  fi
+}
 
-# setup encryption
-cryptsetup luksFormat -c aes-xts-plain64 -s 512 ${device}3
-cryptsetup open ${device}3 base
+partion_disk() {
+	# DESTroying mbr or gpt
+	sgdisk -Z $DEVICE
+	# creating new gpt table
+	sgdisk -og $DEVICE
+	# refresh os table partition information
+	partprobe $DEVICE
+	# setup partitions
+	sgdisk -n 1:2048:1026047 -c 1:"EFI System Partition" -t 1:ef00 $DEVICE
+	sgdisk -n 2:1026048:3074047 -c 2:"Linux /boot" -t 2:8300 $DEVICE
+	# get end of disk
+	local end_sector=$(sgdisk -E $DEVICE)
+	sgdisk -n 3:3074048:${end_sector} -c 3:"Linux LVM" -t 3:8e00 $DEVICE
+	# print partition table
+  sgdisk -p ${DEVICE}
+}
 
-# initializing LVM volume
-pvcreate -f /dev/mapper/base
-# creating volume group
-vgcreate ${group} /dev/mapper/base
-# creating LVM partitions
-lvcreate -L 50G -n root ${group}
-lvcreate -L 2G -C y -n swap ${group}
-lvcreate -l 100%FREE -n home ${group}
+format_disk() {
+	local boot_disk=$(find -L /dev/disk/by-path -samefile ${DEVICE}2)
+	local efi_boot_disk=$(find -L /dev/disk/by-path -samefile ${DEVICE}1)
 
-# creating filesystem for partitions
-mkfs.ext4 -F -L archsystem /dev/mapper/${group}-root
-mkfs.ext4 -F -L archuser /dev/mapper/${group}-home
-mkswap /dev/mapper/${group}-swap
-boot_disk=`find -L /dev/disk/by-path -samefile ${device}2`
-efi_boot_disk=`find -L /dev/disk/by-path -samefile ${device}1`
-mkfs.ext4 -F -L boot ${boot_disk}
-mkfs.fat -F32 ${efi_boot_disk}
+	# setup encryption
+	cryptsetup luksFormat -c aes-xts-plain64 -s 512 ${DEVICE}3
+	cryptsetup open ${DEVICE}3 base
 
-# mouting volumes
-mkdir -p ${dest}
-mount /dev/mapper/${group}-root ${dest}
-mkdir -p ${dest}/boot
-mount ${boot_disk} ${dest}/boot
-mkdir -p ${dest}/boot/efi
-mount ${efi_boot_disk} ${dest}/boot/efi
-mkdir -p ${dest}/home
-mount /dev/mapper/${group}-home ${dest}/home
+	# initializing LVM volume
+	pvcreate -f /dev/mapper/base
+	# creating volume GROUP
+	vgcreate ${GROUP} /dev/mapper/base
+	# creating LVM partitions
+	lvcreate -L 50G -n root ${GROUP}
+	lvcreate -L 2G -C y -n swap ${GROUP}
+	lvcreate -l 100%FREE -n home ${GROUP}
 
-# installing basic system
-pacstrap ${dest} base
+	# creating filesystem for partitions
+	mkfs.ext4 -L archsystem /dev/mapper/${GROUP}-root
+	mkfs.ext4 -L archuser /dev/mapper/${GROUP}-home
+	mkswap /dev/mapper/${GROUP}-swap
+	mkfs.ext4 -L boot ${boot_disk}
+	mkfs.fat -F32 ${efi_boot_disk}
+}
 
-# enabling swap
-swapon /dev/mapper/${group}-swap
+mount_volumes() {
+	# mouting volumes
+	local efi_boot_disk=$(find -L /dev/disk/by-path -samefile ${DEVICE}1)
+	mkdir -p ${DEST}
+	mount /dev/mapper/${GROUP}-root ${DEST}
+	mkdir -p ${DEST}/boot
+	mount ${boot_disk} ${DEST}/boot
+	mkdir -p ${DEST}/boot/efi
+	mount ${efi_boot_disk} ${DEST}/boot/efi
+	mkdir -p ${DEST}/home
+	mount /dev/mapper/${GROUP}-home ${DEST}/home
+}
 
-# generating fstab
-genfstab -U -p ${dest} >> ${dest}/etc/fstab
+install_basic() {
+	# installing basic system
+	pacstrap ${DEST} base
 
-# adding swap to fstab
-echo "/dev/mapper/${group}-swap swap swap defaults 0 0" >> ${dest}/etc/fstab
+	# generating fstab
+	genfstab -U -p ${DEST} >> ${DEST}/etc/fstab
 
-# getting chroot installation script
-(cd $dest/root && curl -O https://raw.githubusercontent.com/alexandre-merle/arch-install/master/chroot-install.sh)
+	# adding swap to fstab
+	echo "/dev/mapper/${GROUP}-swap swap swap defaults 0 0" >> ${DEST}/etc/fstab
 
-mkdir ${dest}/hostrun
-mount --bind /run ${dest}/hostrun
+	# getting chroot installation script
+	(cd $DEST/root && curl -O https://raw.githubusercontent.com/alexandre-merle/arch-install/master/chroot-install.sh)
+}
 
-# chroot to the new system and run installation script
-arch-chroot $dest /bin/bash /root/chroot-install.sh ${device} ${group}
+lvm_init() {
+	mkdir ${DEST}/hostrun
+	mount --bind /run ${DEST}/hostrun
+}
 
-umount ${dest}/hostrun
+launch_chroot() {
+	# chroot to the new system and run installation script
+	arch-chroot $DEST /bin/bash /root/chroot-install.sh ${DEVICE} ${GROUP}
+}
 
-# umount all partitions
-umount -R ${dest}
+unmount() {
+	umount ${DEST}/hostrun
+
+	# umount all partitions
+	umount -R ${DEST}
+	echo "Installation finished"
+}
+
+main() {
+	test_connection
+	check_args
+	partion_disk
+	format_disk
+	mount_volumes
+	lvm_init
+	umount
+}
+
+main
